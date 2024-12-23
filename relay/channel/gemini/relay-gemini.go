9f0db70243666df39bc7ae3b824cc9ebc1fea07a
yuqu.li <yuqi.li.91@gmail.com>
2024-12-24 01:33:04 +0800

    disable system instruct

--- relay/channel/gemini/relay-gemini.go
+++ relay/channel/gemini/relay-gemini.go
@@ -99,16 +99,7 @@ func CovertGemini2OpenAI(textRequest dto.GeneralOpenAIRequest) (*GeminiChatReque
 	//shouldAddDummyModelMessage := false
 	for _, message := range textRequest.Messages {
 
-		if message.Role == "system" {
-			geminiRequest.SystemInstructions = &GeminiChatContent{
-				Parts: []GeminiPart{
-					{
-						Text: message.StringContent(),
-					},
-				},
-			}
-			continue
-		} else if message.Role == "tool" {
+		if message.Role == "tool" {
 			if len(geminiRequest.Contents) == 0 || geminiRequest.Contents[len(geminiRequest.Contents)-1].Role != "user" {
 				geminiRequest.Contents = append(geminiRequest.Contents, GeminiChatContent{
 					Role: "user",
@@ -194,6 +185,11 @@ func CovertGemini2OpenAI(textRequest dto.GeneralOpenAIRequest) (*GeminiChatReque
 		// there's no assistant role in gemini and API shall vomit if Role is not user or model
 		if content.Role == "assistant" {
 			content.Role = "model"
+		}
+		
+		// Converting system prompt to prompt from user for the same reason
+		if content.Role == "system" {
+			content.Role = "user"
 		}
 		geminiRequest.Contents = append(geminiRequest.Contents, content)
 	}
